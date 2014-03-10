3c58aa533c9a08b1afa3047bbb8d367dffa91567
Olaf Veerman <olaf.veerman@gmail.com>
2014-03-10 13:03:35 +0000

    First dirty alpha prototype of PH org finder

--- data-wrangling/org-markdown.py
+++ data-wrangling/org-markdown.py
@@ -13,9 +13,8 @@
 
 # TODO
 # Checks on the output folder  // If not empty, ask user if we should continue anyway
-# Skip CSV header on for
 # Replace spaces for hyphens in the title (just in case)
-# Make sure there are no colons in the YAML variables
+# Indent all paragraphs of the mission statement
 
 import csv
 import sys
@@ -24,22 +23,29 @@ import arrow
 folder_out = sys.argv[1] 
 file_in = sys.argv[2]
 
-# Fetch the index of each relevant column
-# This allows for some flexibility in column position
-# and does a check if all mandatory columns are present
+
+def indent(n = 1):
+	base_indentation = '  '
+	return base_indentation * n
+
 
 with open(file_in, 'rb') as ifile:
+
+	# Fetch the index of each relevant column. This allows for some flexibility
+	# in column position and does a check if all mandatory columns are present
 	reader = csv.reader(ifile)
-	header = list(reader)[0]
+	header = reader.next()
 
 	# Dict with the variable names (key) and the relevant column names (value)
 	cols = { 'oid' : 'ORG ID', 'name' : 'Organization_name', 'add' : 'Address', 'cit' : 'City', 'st' : 'State', 'zip' : 'Zip', 'locs' : 'Location_of_services', 'loco' : 'Location_of_offices', 'web': 'Website', 'keyw' : 'Keywords', 'imp' : 'Impact Area', 'miss' : 'Org Mission', 'descr' : 'Organization_overview', 'cash' : 'Cash_grants', 'cash1' : 'Description_of_cash_grants_1', 'cash2' : 'Description_of_cash_grants_2', 'serv' : 'Service_opportunities', 'serv1' : 'Description_of_service_opportunity_1', 'serv2' : 'Description_of_service_opportunity_2', 'learn' : 'How_can_you_help_students_learn_more_about_you', 'cont' : 'How_can_you_continue_the_relationship_afterwards', 'sal' : 'Salutation', 'first' : 'First_Name', 'last' : 'Last_Name', 'title' : 'Title', 'intro' : 'Introduction', 'ph' : 'Phone', 'ext' : 'Extension', 'fax' : 'Fax', 'email' : 'Email', 'meth' : 'Preferred_Method_of_Contact', 'time' : 'Best_Time_to_Contact', 'lat' : 'lat', 'lng' : 'lng' }
 	fields = { }
 	error = False
 
-	# Loop over key & value of the 'cols' dict and build the index
+	# Loop over key & value of the 'cols' dict and build the 'fields' dict with 
+	# convenient variable name (key) and the position of the column in the CSV 
+	# (value)
+
 	for key,value in cols.iteritems():	
-		"Build the 'fields' dict with convenient variable name (key) and the position of the column in the CSV (value)"
 		try:
 			fields[key] = header.index(value)
 		except ValueError:
@@ -50,27 +56,40 @@ with open(file_in, 'rb') as ifile:
 		print "Please check your CSV and try again."
 		sys.exit(0)
 
-with open(file_in, 'rb') as ifile:
-	reader = csv.reader(ifile)
+	utc = arrow.utcnow()
+	timestamp = utc.format('YYYY-MM-DD')
 
+	# Generate a markdown file for each row in the CSV
 	for row in reader:
-		"Generate a markdown file for each row in the CSV"
+
+		# Replace any colon in the title for a hyphen
+		row[fields['name']] = row[fields['name']].replace(':', '-')
+
+		# Replace any carriage return (_x000D_) with a newline (\n)
+		row[fields['descr']] = row[fields['descr']].replace('_x000D_', '\n')
+
+		# To be able to include the mission as a YAML variable, we need to indent all the paragraphs of the mission statement.
 
 		# Create file with proper file-name. Jekyll requires every file to start
 		# with a date
-		utc = arrow.utcnow()
-		timestamp = utc.format('YYYY-MM-DD-')
-		file_out = folder_out + '/' + timestamp + row[fields['oid']] + '.md'
+		file_out = folder_out + '/' + timestamp + '-' + row[fields['oid']] + '.md'
+
 		f = open(file_out, 'w')
 
 		f.write('---\n')
+		f.write('layout: organization\n')
+		f.write('category: organization\n')
+		f.write('\n')
 		f.write('title: ' + row[fields['name']] + '\n')
 		f.write('impact_area: ' + row[fields['imp']] + '\n')
-		f.write('theme: ' + row[fields['keyw']] + '\n')
+		f.write('keywords: ' + row[fields['keyw']] + '\n')
 		f.write('coordinates: ' + row[fields['lat']] + ',' + row[fields['lng']] + '\n')
+		#f.write('mission: |\n')
+		#f.write(indent() + row[fields['miss']] + '\n')
+		f.write('address: |\n')
+		f.write(indent() + row[fields['add']] + '\n')
+		f.write(indent() + row[fields['cit']] + ' ' + row[fields['st']] + ' ' + row[fields['zip']]+ '\n')
 		f.write('---\n')
-		f.write('Organization\'s mission\n')
-		f.write(row[fields['miss']] + '\n\n')
 		f.write('Organization\'s overview\n')
 		f.write(row[fields['descr']])
 		
