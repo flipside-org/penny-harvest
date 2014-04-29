b57edc9abcd7b1365bc8a007bf1174e826cf3823
Daniel Silva <daniel.silva@flipside.org>
2014-04-29 17:15:30 +0100

    Move PH_filters object to separate file. First work on national filtering

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,159 +1,12 @@
 $(document).foundation();
 
-(function($) {
-  $.fn.activate = function() {
-    return this.addClass('on');
-  };
-  $.fn.activateToggle = function() {
-    return this.toggleClass('on');
-  };
-  $.fn.deactivate = function() {
-    return this.removeClass('on');
-  };
-  $.fn.is_active = function() {
-    return this.hasClass('on');
-  };
-  $.fn.getFilterId = function() {
-    return this.attr('data-filter-id');
-  };
-  $.fn.getByFilterId = function(id) {
-    return this.filter('[data-filter-id="' + id + '"]');
-  };
-  $.fn.getActivated = function() {
-    return this.filter('.on');
-  };
-})(jQuery);
-
-/**
- * Static object to setup filters.
- */
-var PH_filters = {
-  // Default settings.
-  settings : {
-    cb_click_impact_area : function() {},
-    cb_initialize : function() {},
-  },
-  
-  // Store the impact_area filters
-  impact_areas : [],
-  // Store keywords filters
-  keywords : [],
-  
-  /**
-   * Init function.
-   */
-  init : function(options) {
-    // Extend settings
-    PH_filters.settings = $.extend({}, PH_filters.settings, options);
-    
-    // Get impact_areas filters
-    PH_filters.impact_areas = $('[data-filter-type="impact_area"]');
-    // Get keywords filters
-    PH_filters.keywords = $('[data-filter-type="keyword"]');
-    
-    // Click listeners to activate.
-    PH_filters.impact_areas.click(function(e) {
-      e.preventDefault();
-      $(this).activateToggle();
-      
-      PH_filters.settings.cb_click_impact_area.apply(this);
-      
-      // Save state.
-      PH_filters.save_to_url();
-    });
-    
-    PH_filters.keywords.click(function(e) {
-      e.preventDefault();
-      $(this).activateToggle();
-            
-      // Save state.
-      PH_filters.save_to_url();
-    });
-    
-    // Load filters from url.
-    PH_filters.load_from_url();
-    
-    PH_filters.settings.cb_initialize();
-  },
-  
-  /**
-   * Returns the active filters of the given type in an array.
-   * Only returns the filter id
-   * @param string type
-   *   Filter type.
-   * @return array
-   *   Filter id
-   */
-  get_active: function(type) {
-    // Empty jQuery object to start with.
-    var active = [];
-    switch (type) {
-      case 'impact_areas':
-        PH_filters.impact_areas.getActivated().each(function() {
-          active.push($(this).getFilterId());
-        });
-        break;
-      case 'keywords':
-        PH_filters.keywords.getActivated().each(function() {
-          active.push($(this).getFilterId());
-        });
-        break;
-    }
-    return active;
-  },
-  
-  /**
-   * Save application state to the url in Base64 encoded format.
-   */
-  save_to_url: function() {
-    // Use keys as small as possible to shrink encoded string.
-    var data = {
-      // impact_areas
-      i: PH_filters.get_active('impact_areas'),
-      // keywords
-      k: PH_filters.get_active('keywords')
-    };
-    
-    if (data.i.length === 0 && data.k.length === 0) {
-      location.hash = null;
-    }
-    else {
-      // Encode and save to URL.
-      var encoded = Base64.encodeObj(data);
-      location.hash = encoded;
-    }
-    
-  },
-  
-  /**
-   * Loads the application status from the Url.
-   */
-  load_from_url: function() {
-    if (!location.hash)
-      return false;
-    
-    // Load hash from url.
-    var hash = location.hash.substring(1);
-    var data = Base64.decodeObj(hash);
-    
-    if (data === null)
-      return false;
-    
-    // Reset app status.
-    // Impact areas.
-    $.each(data.i, function(i, v) {
-      PH_filters.impact_areas.getByFilterId(v).activate();
-    });
-    
-    $.each(data.k, function(i, v) {
-      PH_filters.keywords.getByFilterId(v).activate();
-    });
-  },
-};
 
 
 
 
+
+
+if ($('#map').length) {
 
 
 $.get('/json/local-orgs.geojson', function(geoJson){
@@ -177,30 +30,189 @@ $.get('/json/local-orgs.geojson', function(geoJson){
 PH_filters.init({
   // Callback for when a impact area is clicked.
   cb_click_impact_area : function() {
-    var impact_areas = PH_filters.get_active('impact_areas');
     
+    var selected_impact_areas = PH_filters.get_active('impact_areas');
     map.featureLayer.setFilter(function(f) {
       // When no impact area return all.
-      if (impact_areas.length)
-        return $.inArray(f.properties.impact_area, impact_areas) >= 0;
+      if (selected_impact_areas.length)
+        return $.inArray(f.properties.impact_area, selected_impact_areas) >= 0;
       else
         return true;
     });
       
   },
-  // Callback after initialization.
-  cb_initialize : function() {
-    var impact_areas = PH_filters.get_active('impact_areas');
+  // Callback for when a keyword is clicked.
+  cb_click_keyword : function() {
     
+    var selected_keywords = PH_filters.get_active('keywords');
     map.featureLayer.setFilter(function(f) {
       // When no impact area return all.
-      if (impact_areas.length)
-        return $.inArray(f.properties.impact_area, impact_areas) >= 0;
-      else
-        return true;
+      if (selected_keywords.length) {
+        // Keywords are exclusive, meaning that all of them must be present.
+        for (var i in selected_keywords) {
+          if ($.inArray(selected_keywords[i], f.properties.keywords) == -1) {
+            return false;
+          }
+        }
+      }
+      return true;
     });
+  },
+  
+  // Callback after initialization.
+  cb_initialize : function() {
+    var selected_keywords = PH_filters.get_active('keywords');
+    var selected_impact_areas = PH_filters.get_active('impact_areas');
+    
+    // If nothing is active show all.
+    if (!selected_keywords.length && !selected_impact_areas.length) {
+      map.featureLayer.setFilter(function(f) { return true; });
+    }
+    // Priority to the impact areas. 
+    else if (selected_impact_areas.length) {
+      map.featureLayer.setFilter(function(f) {
+        return $.inArray(f.properties.impact_area, selected_impact_areas) >= 0;
+      });
+    }
+    // Keywords are last.
+    else if (selected_keywords.length) {
+      map.featureLayer.setFilter(function(f) {
+        // Keywords are exclusive, meaning that all of them must be present.
+        for (var i in selected_keywords) {
+          if ($.inArray(selected_keywords[i], f.properties.keywords) == -1) {
+            return false;
+          }
+        }
+        return true;
+      });
+    }
   },
 });
 
 
+},'json');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$.get('/json/national-orgs.json', function(national_orgs){  
+  
+  var source = $("#national-entry").html();
+  var template = Handlebars.compile(source);
+  
+  var page_size = 2;
+  
+  var render = function(orgs) {
+    var html = template({"orgs" : orgs});
+    $('#national-orgs').html(html);
+    
+    // SHow list button.
+    var $cards = $(".national > li");
+    if ($cards.length > page_size) {
+      $('#trigger-show').removeAttr('disabled');
+    }
+    else {
+      $('#trigger-show').attr('disabled', '');
+    }
+    // Hide cards.
+    $(".national > li").slice(page_size).addClass('hide');
+  };
+  
+  $('#trigger-show').click(function() {
+    $(".national > li.hide").slice(0, page_size).removeClass('hide');
+     
+    // Enable/Disable show list button based on cards.
+    if ($(".national > li.hide").length == 0) {
+      // Nothing else to show. Disable button.
+      $('#trigger-show').attr('disabled', '');
+    }
+    else {
+      $('#trigger-show').removeAttr('disabled');
+    }
+  });
+  
+  
+  // Initialize.
+  PH_filters.init({
+    // Callback for when a impact area is clicked.
+    cb_click_impact_area : function() {
+      var selected_impact_areas = PH_filters.get_active('impact_areas');
+      
+      if (selected_impact_areas.length) {
+        var filtered = $.grep(national_orgs, function(v) {
+          return $.inArray(v.impact_area, selected_impact_areas) >= 0;
+        });
+        render(filtered);
+      }
+      else {
+        render(national_orgs);
+      }
+      
+    },
+    // Callback for when a keyword is clicked.
+    cb_click_keyword : function() {
+      var selected_keywords = PH_filters.get_active('keywords');
+      
+      if (selected_keywords.length) {
+        var filtered = $.grep(national_orgs, function(v) {
+          // Keywords are exclusive, meaning that all of them must be present.
+          for (var i in selected_keywords) {
+            if ($.inArray(selected_keywords[i], v.keywords) == -1) {
+              return false;
+            }
+          }
+          return true;
+        });
+        render(filtered);
+      }
+      else {
+        render(national_orgs);
+      }
+      
+    },
+    // Callback after initialization.
+    cb_initialize : function() {
+      var selected_keywords = PH_filters.get_active('keywords');
+      var selected_impact_areas = PH_filters.get_active('impact_areas');
+      
+      // If nothing is active show all.
+      if (!selected_keywords.length && !selected_impact_areas.length) {
+        render(national_orgs);
+      }
+      // Priority to the impact areas. 
+      else if (selected_impact_areas.length) {
+        var filtered = $.grep(national_orgs, function(v) {
+          return $.inArray(v.impact_area, selected_impact_areas) >= 0;
+        });
+        render(filtered);
+      }
+      // Keywords are last.
+      else if (selected_keywords.length) {
+        var filtered = $.grep(national_orgs, function(v) {
+          // Keywords are exclusive, meaning that all of them must be present.
+          for (var i in selected_keywords) {
+            if ($.inArray(selected_keywords[i], v.keywords) == -1) {
+              return false;
+            }
+          }
+          return true;
+        });
+        render(filtered);
+      }
+      
+    },
+  });
+  
 },'json');
